97ece2b90911aecdacb2cb5e1e539ff379cf72cd
ADOwen <92840021+ADOwen@users.noreply.github.com>
2025-04-04 11:55:33 -0500

    add padding to h1 and update page title and description

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center">
-      <h1>Coffee Connoisseur</h1>
+    <main className="flex flex-col gap-[32px] row-start-2 items-center pt-14">
+      <h1 >Coffee Connoisseur</h1>
     </main>
   );
 }
