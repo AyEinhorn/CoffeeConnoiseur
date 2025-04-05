5d5cb704eef84521f33ee389384797e6c0d617b4
ADOwen <92840021+ADOwen@users.noreply.github.com>
2025-04-04 20:40:04 -0500

    add banner component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Banner from "@/components/banner.client";
+
 export default function Home() {
+  
   return (
     <main className="flex flex-col gap-[32px] row-start-2 items-center pt-14">
-      <h1 >Coffee Connoisseur</h1>
+      <Banner />
     </main>
   );
 }
