0336769bf658432f5c7a8be3efb49a57edd60f07
ADOwen <92840021+ADOwen@users.noreply.github.com>
2025-04-05 14:54:05 -0500

    update banner.client import

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Banner from "@/components/banner.client";
+import Banner from "../components/banner.client";
 
 export default function Home() {
   
   return (
     <main className="flex flex-col gap-[32px] row-start-2 items-center pt-14">
       <Banner />
+      
     </main>
   );
 }
